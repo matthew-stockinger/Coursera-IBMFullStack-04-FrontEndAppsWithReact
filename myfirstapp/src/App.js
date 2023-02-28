710b8ea511b598d4c77884ebc8710ee270ec3609
Matthew Stockinger <matt@matthewstockinger.com>
2023-02-28 14:48:50 -0600

    add dynamic date

--- myfirstapp/src/App.js
+++ myfirstapp/src/App.js
@@ -1,4 +1,13 @@
 function App() {
-  return <h1>Hello World!</h1>;
+  let dateToday = new Date();
+  return (
+    <div>
+      <h1>Hello world! From Matt Stockinger!</h1>
+      <h2>
+        The time now is {dateToday.toLocaleTimeString()}. The date is{" "}
+        {dateToday.getDay()}, {dateToday.getMonth()} {dateToday.getDate()}.
+      </h2>
+    </div>
+  );
 }
 export default App;
--- myfirstapp/src/index.js
+++ myfirstapp/src/index.js
@@ -1,7 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <App />
-);
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+function ref() {
+  root.render(<App />);
+}
+
+setInterval(ref, 1000);
