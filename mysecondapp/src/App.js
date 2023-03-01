46fe65872a19d2e4562ccaf414566e7b1c6f658d
Matthew Stockinger <matt@matthewstockinger.com>
2023-03-01 09:12:09 -0600

    recreate myfirst app, some from memory

---
+++ mysecondapp/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Hello world!</h1>
+      <p>The current local time is {new Date().toLocaleTimeString()}.</p>
+    </div>
+  );
+}
+
+export default App;
