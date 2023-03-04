0c100485b56c1f0175b13f9a8de051ad5b2478c2
Matthew Stockinger <matt@matthewstockinger.com>
2023-03-04 11:57:18 -0600

    add practice click counter and form

--- mysecondapp/src/App.js
+++ mysecondapp/src/App.js
@@ -1,28 +1,76 @@
+import React from "react";
+
 const App = () => {
-  const dateObj = new Date();
   return (
     <div>
-      <h1>Hello world!</h1>
-      <h2>The current date is {dateObj.toLocaleDateString()}.</h2>
-      <h2>The current time is {dateObj.toLocaleTimeString()}.</h2>
+      <Clicker />
+      <EmPwdForm />
+    </div>
+  );
+};
+
+///////// Practice Form ///////////////////////
+const EmPwdForm = () => {
+  const [email, setEmail] = React.useState("");
+  const [pwd, setPwd] = React.useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(`Email = ${email}, Password = ${pwd}`);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <h1>Registration</h1>
+        <label>
+          Email:
+          <input
+            name="email"
+            type="email"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+            required
+          />
+        </label>
+
+        <label>
+          Password:
+          <input
+            name="pwd"
+            type="password"
+            value={pwd}
+            onChange={(event) => setPwd(event.target.value)}
+            required
+          />
+        </label>
+
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+///////////// Practice - Function component with React Hook //////////////
+const Clicker = () => {
+  const [numberOfClicks, setNumberOfClicks] = React.useState(0);
+
+  return (
+    <div>
+      <h1>A Button That Counts Its Clicks</h1>
+      <button onClick={() => setNumberOfClicks(numberOfClicks + 1)}>
+        Click Me!
+      </button>
+      <br />
+      <p>
+        You have clicked this button {numberOfClicks} time
+        {numberOfClicks == 1 ? "" : "s"}.
+      </p>
+      <p>
+        <button onClick={() => setNumberOfClicks(0)}>Reset</button>
+      </p>
     </div>
   );
 };
 
 export default App;
-
-// if (keyDown("space")) {
-//   lowerblink.y = lowerblink.y + 2;
-//   upperblink.y = upperblink.y - 2;
-// } else {
-//   lowerblink.y = lowerblink.y - 2;
-//   upperblink.y = upperblink.y + 2;
-// }
-
-// // make sure blinkers don't move too far.
-// if (lowerblink.y < 200) {
-//   lowerblink.y = 200;
-// }
-// if (lowerblink.y > 400) {
-//   lowerblink.y = 400;
-// }
